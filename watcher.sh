450ecb5704166b60237a2c042834a5775c4b2405
alwinlao <alwinlao@users.noreply.github.com>
2024-04-21 03:02:39 +0000

    init commit

--- watcher.sh
+++ watcher.sh
@@ -1,1 +1,20 @@
-#TODO
+namespace="sre"
+deployment="swype-app"
+restart_limit=3
+
+loop=true
+
+while $loop; do
+  restart_count=$(kubectl get pods -n $namespace | awk "/$deployment/{print \$4}") 
+  echo "$deployment restart_count is $restart_count"
+  if (($restart_count>$restart_limit)); then
+    echo "$deployment restart_count $restart_count is greater than restart_limit $restart_limit."
+    kubectl scale --replicas=0 deployment/$deployment -n $namespace
+    echo "$deployment scaled down to 0."
+    loop=false
+    echo "loop exited."
+    break
+  fi
+  echo "sleep 60s."
+  sleep 60
+done
